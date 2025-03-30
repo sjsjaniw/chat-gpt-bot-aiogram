3dce1ff3ab200bd60367fc4d7eecc0b3ae138902
sjsjaniw <sjsjaniw@gmail.com>
2025-03-30 20:33:41 +0300

    refactoring

--- ai.py
+++ ai.py
@@ -1,25 +1,24 @@
 from g4f.client import AsyncClient
 from g4f.Provider import Blackbox
+from aiofiles.os import remove
 import aiofiles
 import io
-import redis
+import redis.asyncio as redis
 import json
 
+client = AsyncClient()
+redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
-ai_list = ["o3-mini"]
-
-r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 async def add_message(key: str, role: str, content: str) -> None:
     message = {"role": role, "content": content}
-    r.rpush(key, json.dumps(message))
+    await redis_client.rpush(key, json.dumps(message))
 
 async def get_all_messages(key: str) -> list:
-    raw_messages = r.lrange(key, 0, -1)
+    raw_messages = await redis_client.lrange(key, 0, -1)
     return [json.loads(msg) for msg in raw_messages]
     
 async def response_to_ai(tg_id: int, text: str, image_path: str | None = None, web_search: bool = False) -> str:
-    client = AsyncClient()
     key = f"messages_{tg_id}"
 
     if text == None: text = ""
@@ -28,12 +27,12 @@ async def response_to_ai(tg_id: int, text: str, image_path: str | None = None, w
     
     image = None
     if image_path: 
-        with open(image_path, "rb") as image:
-            image_data = image.read()
+        with aiofiles.open(image_path, "rb") as image:
+            image_data = await image.read()
         image_io = io.BytesIO(image_data)
         
         response = client.chat.completions.create(
-            model=ai_list[0],
+            model="o3-mini",
             provider=Blackbox,
             messages=messages,
             web_search=web_search,
@@ -41,7 +40,7 @@ async def response_to_ai(tg_id: int, text: str, image_path: str | None = None, w
             stream=True)      
     else:
         response = client.chat.completions.create(
-            model=ai_list[0],
+            model="o3-mini",
             provider=Blackbox,
             messages=messages,
             web_search=web_search,
@@ -49,13 +48,12 @@ async def response_to_ai(tg_id: int, text: str, image_path: str | None = None, w
             stream=True)
         
     if image_path: 
-        await aiofiles.os.remove(path=image_path)
+        await remove(path=image_path)
     
     full_response = ""
     async for completion in response:
         chunk = completion.choices[0].delta.content or ""
         full_response += chunk
-    
-    messages = await get_all_messages(key)
+
     await add_message(key, "assistant", full_response)
     return full_response
